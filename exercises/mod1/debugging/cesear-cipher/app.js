e9a71767034d92caf4f90670149c539b31b26e50
Kenny Souvannaleut <kennysouvannaleut@Kennys-MBP.localdomain>
2019-12-10 17:25:07 -0700

    worked on es6 and continued debugging using readline, nodeJs

--- exercises/mod1/debugging/cesear-cipher/app.js
+++ exercises/mod1/debugging/cesear-cipher/app.js
@@ -1,31 +1,34 @@
 // CAESAR CIPHER
 
-var readline = require('readline-sync');
-var input = readline.question('What phrase would you like to encrypt? ').toLowerCase();
-var shift = parseInt(readline.question('How many letters would you like to shift? '));
+var readline = require("readline-sync");
+var input = readline.question('What phrase would you like to encrypt?').toLowerCase()
+var shift = parseInt(readline.question('How many letters would you like to shift?'));
 
-function caesarCipher(str, k) {
-    let result = '';
 
-    for (let i = 0; i < str.length; i++) {
-    const charCode = str.charCodeAt(i);
+const result = ''
+function cesearCipher (string, number) {
 
-    if (charCode < 65 || charCode > 122 ||
-       (charCode > 90 && charCode < 97)
-       ) { 
-            result += str[i];
-        } else {
-            let newCharCode = charCode + Math.ceil(k % 26); 
+    for (let i = 0; i < string.length; i++) {
+    const code = string.charCodeAt[i];
 
-            if (charCode >= 97 && newCharCode > 122) {
-                newCharCode = newCharCode - 122 + 96;
-            }
-            if (charCode <= 90 && newCharCode > 90) {
+    if (code < 65 || code > 122 || (code > 90 && code < 97)) 
+            { 
+            result += string.charCodeAt
+            } else {
+            let newCharCode = code + Math.ceil(number % 26); 
+
+            if (code >= 97 && newCharCode > 122) {
+                let newCharCode = code + Math.ceil(number % 26)
+                newCharCode = newCharCode - 122 + 96
+                }
+            if (code <= 90 && newCharCode > 90) {
                 newCharCode = newCharCode - 90 + 64;
             }
-            result += String.fromCharCode(newCharCode);
+            result += string(newCharCode);
         }
     }
-    console.log(result);
-    return result
+        return result;
 }
+ console.log(result)
+
+
--- exercises/mod1/setup/node-calculator/app.js
+++ exercises/mod1/setup/node-calculator/app.js
@@ -1,4 +1,62 @@
 // NODE CALCULATOR
 
-const readline = require("readline-sync");
+var readlineSync = require('readline-sync');
+    operators = ['+', '-', '*', '/'],
+    index = readlineSync.keyInSelect(operators, 'Please choose the type of operation to perform');
+    console.log('Great!' + operators[index], 'Please stand by for further instructions');
 
+const readline = require('readline');
+const r1 = readline.createInterface ({
+    input: process.stdin,
+    output: process.stdout
+});
+
+r1.question('Please enter your first number:', (num1) => {
+    r1.question('Please enter your second number:', (num2) => {
+        const operators = {
+            calculateProduct(...rest) {
+                console.log('Please use the multiply method')
+                return this.multiply(...rest);
+            },
+            multiply(num1, num2) {
+                return num1 * num2
+            }
+        }; 
+        console.log(num1*num2); 
+    // var result = []
+    // console.log(`The sum of the two numbers is ${(+num1 + (+num2))}`);
+    // console.log(`The diff of the two numbers is ${num1 - num2}`);
+    // console.log(`The mul of the two numbers is ${num1 * num2}`);
+    // console.log(`The div of the two numbers is ${num1 / num2}`);
+    // console.log(`The result is ${result}`);
+    r1.close();
+    })
+})
+
+                
+            
+
+
+
+
+
+
+// let add = function(a, b) {
+//     return a + b;
+// }
+// console.log(add(1, 1));
+
+// let sub = function(a, b) {
+//     return a - b;
+// }
+// console.log(sub(10, 5));
+
+// let mul = function(a, b) {
+//     return a * b;
+// }
+// console.log(mul(2, 3));
+
+// let div = function(a, b) {
+//     return a / b;
+// }
+// console.log(div(5, 5));
