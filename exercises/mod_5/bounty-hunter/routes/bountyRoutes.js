6579b33b68d31ec9001d66507a2730083d1100c5
Kenny Souvannaleut <ksouvannaleut@programmer.net>
2020-02-21 15:51:39 -0700

    worked with node express

--- exercises/mod_5/bounty-hunter/routes/bountyRoutes.js
+++ exercises/mod_5/bounty-hunter/routes/bountyRoutes.js
@@ -1,43 +1,15 @@
-const express = require("express");
+const express = require('express');
 const bountyRouter = express.Router();
-const uuid = require("uuid/v4");
+const uuid = require('uuid/v4');
 
 const bounties = [
-    {   
-        FirstName: "Evil", 
-        LastName: "K", 
-        Living: "true", 
-        BountyAmount: "5000", 
-        Type: "Sith", 
-        _id: uuid() 
-    },
-    {
-        FirstName: "Dead", 
-        LastName: "A", 
-        Living: "false", 
-        BountyAmount: "1000", 
-        Type: "Jedi", 
-        _id: uuid() 
-    },
-    {
-        FirstName: "Killer", 
-        LastName: "B", 
-        Living: "false", 
-        BountyAmount: "2626", 
-        Type: "Sith", 
-        _id: uuid() 
-    },
-    {
-        FirstName: "Bad", 
-        LastName: "T", 
-        Living: "true", 
-        BountyAmount: "100", 
-        Type: "Jedi", 
-        _id: uuid() 
-    }
+    {firstName: 'Mini', lastName: 'Me', living: 'true', bounty: '$5,000.00', type: 'Sith', _id: uuid() },
+    {firstName: 'Deadman', lastName: 'Walking', living: 'false', bounty: '$100.00', type: 'Jedi', _id: uuid() },
+    {firstName: 'Killer', lastName: 'Instincts', living: 'false', bounty: '$2,626.00', type: 'Sith', _id: uuid() },
+    {firstName: 'Cool', lastName: 'Guy', living: 'true', bounty: '$100,000.00', type: 'Jedi', _id: uuid() }
 ];
 
-bountyRouter.route("/")
+bountyRouter.route('/')
     .get((req, res) => {
         res.send(bounties);
     })
@@ -48,13 +20,13 @@ bountyRouter.route("/")
         res.send(`${newBounty.firstName} has been added to the database!`);
 });
 
-bountyRouter.route("/:bountyId")
+bountyRouter.route('/:bountyId')
     .get((req, res) => {
         res.send(`GET on /bounty/${req.params.bountyId} endpoint`);
-})
+    })
     .put((req, res) => {
         res.send(`PUT on /bounty/${req.params.bountyId} endpoint`);
-})
+    })
     .delete((req, res) => {
         res.send(`DELETE on /bounty/${req.params.bountyId} endpoint`);
 });
--- exercises/mod_5/bounty-hunter/server.js
+++ exercises/mod_5/bounty-hunter/server.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 9000;
+const port = process.env.port || 8000;
 
 app.use(express.json());
 
-app.use("/bounty", require("./routes/bountyRoutes"));
+app.use('/bounty', require('./routes/bountyRoutes'));
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
