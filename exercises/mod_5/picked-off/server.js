204d3470bb566991d371251ddec43c6d2f2e1e6a
Kenny Souvannaleut <ksouvannaleut@programmer.net>
2020-02-24 14:12:56 -0700

    worked on express middleware and next

--- exercises/mod_5/picked-off/server.js
+++ exercises/mod_5/picked-off/server.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const app = express();
+const uuid = require('uuid/v4');
+
+// app.use(express.json());
 
 const bodyParser = require('body-parser');
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/item', require('./routes/items'));
-
-app.use(function(req, res, next) {
-    const err = new Error('Item Not Found');
-    err.status = 404;
-    next(err);
+app.use('/items', (req, res, next) => {
+    console.log('THE ITEMS MIDDLEWARE WAS EXECUTED SUCCESSFULLY');
+    next();
 });
 
+app.use((req, res, next) => {
+    req.body = { name: 'Ninja' }
+    next();
+});
+
+app.get('/items', (req, res, next) => {
+    console.log('GET REQUEST RECEIVED')
+    res.send(req.body)
+})
+
 const port = process.env.PORT || 9000;
-    app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Express app is listening on port ${port}`);
 });
