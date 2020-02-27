875456a0f1e139c9ac39fd2cf5005d3bca135cdf
Kenny Souvannaleut <ksouvannaleut@programmer.net>
2020-02-27 09:40:54 -0700

    worked on mongoose schemas and models to perform CRUD operations

--- exercises/mod_5/bounty-hunter/routes/bountyRoutes.js
+++ exercises/mod_5/bounty-hunter/routes/bountyRoutes.js
@@ -45,7 +45,7 @@ bountyRoutes.route('/:bountyId')
     .put((req, res, next) => {
         Bounty.findOneAndUpdate(
             { _id: req.params.bountyId },
-            (req.body),
+            req.body,
             { new: true },
             (err, updatedBounty) => {
                 if(err) {
@@ -57,14 +57,19 @@ bountyRoutes.route('/:bountyId')
         )
     });
 
-bountyRoutes.get('search/type', (req, res, next) => {
-    Bounty.find({ type: req.query.type }, (err, bounties) => {
-        if(err) {
-            res.status(500)
-            return next(err)
-        };
-        return res.status(200).send(bounties)
-    })
+bountyRoutes.get('search', (req, res, next) => {
+    const { bounty } = req.query;
+    const pattern = new RegExp(bounty);
+    Bounty.find(
+        { type: { $regex: pattern, $options: 'i' } }, 
+        (err, bounties) => {
+            if(err) {
+                res.status(500)
+                return next(err)
+            };
+            return res.status(200).send(bounties)
+        }
+    )
 });
 
 module.exports = bountyRoutes;
